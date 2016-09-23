2bf9216753fd9feb9f69c4582b9788b55d93bf6b
vladimir danilov <vladimir.danilov@dz.ru>
2016-09-23 18:04:11 +0400

    Read a directory with files

--- main.go
+++ main.go
@@ -5,16 +5,16 @@ import (
 	"github.com/nfnt/resize"
 	"image/jpeg"
 	"bytes"
-	"os"
 	"log"
 	"io/ioutil"
+	"strings"
+	//"os"
 )
 
 var BLOCK_SIZE uint = 20
 func main() {
 	fmt.Println("App runing...")
-	img := NewImage("./files/miniony.jpg", "./files/smal-miniony.jpg")
-	img.LoadImage()
+	NewImageList("./files/")
 }
 
 func checkError(e error) {
@@ -22,40 +22,61 @@ func checkError(e error) {
 		log.Fatal(e)
 	}
 }
-
+/**
+Получаем информацию о файле
+ */
 type Image struct {
-	fileName string
-	outFileName string
+	filename string
+	t_data [] int
 }
 
-func NewImage(fileName string, outFileName string) *Image {
+func NewImage(fileName string) *Image {
 	img := new(Image)
-	img.fileName = fileName
-	img.outFileName = outFileName
+	img.filename = fileName
 	return img
 }
 
-func(o *Image) LoadImage()  {
-	fileLoad, fileError := ioutil.ReadFile(o.fileName)
+func(o *Image) Load() *Image {
+	fileLoad, fileError := ioutil.ReadFile(o.filename)
 	checkError(fileError)
 	
 	img, imgError := jpeg.Decode(bytes.NewReader(fileLoad))
 	checkError(imgError)
 	
-	out, err := os.Create(o.outFileName)
-	checkError(err)
-	defer out.Close()
-	
 	small := resize.Resize(BLOCK_SIZE, BLOCK_SIZE, img, resize.Bilinear)
 	bounds := small.Bounds()
-	var t_data []int
+	
 	for x := bounds.Min.X; x < bounds.Max.X; x++ {
 		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 			r, g, b, _ := small.At(x, y).RGBA()
-			t_data = append(t_data, (int(r) + int(g) + int(b)))
+			o.t_data = append(o.t_data, (int(r) + int(g) + int(b)))
 		}
 	}
-	fmt.Println(len(t_data))
-	// write new image to file
-	jpeg.Encode(out, small, nil)
+	return o
+}
+
+/** Подгружаем файлы из директории
+Читаем файлы формата *.jpg
+ */
+type ImageList struct {
+	dirname string
+	images [] Image
+}
+
+func NewImageList(pathname string) ImageList {
+	imageList := new(ImageList)
+	imageList.dirname = pathname
+	imageList.Load()
+	return imageList
+}
+
+func(o *ImageList) Load () *ImageList  {
+	files, _ := ioutil.ReadDir(o.dirname)
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".jpg") {
+			o.images = append(o.images, NewImage(file.Name()).Load())
+		}
+	}
+	fmt.Println(o.images)
+	return o
 }
