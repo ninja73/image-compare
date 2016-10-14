447589594f1554bfde2e3484dc59319b4cc06a0d
vladimir danilov <vladimir.danilov@dz.ru>
2016-10-14 23:00:05 +0400

    Resize image, to gray and Sobel

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/nfnt/resize"
 	"image/jpeg"
 	"bytes"
 	"log"
@@ -11,10 +10,12 @@ import (
 	"os"
 	"math"
 	"strconv"
+	"github.com/disintegration/gift"
+	"image"
 )
 
 const (
-	BLOCK_SIZE uint = 20
+	BLOCK_SIZE int = 20
 	THRESHOLD int = 60
 )
 
@@ -26,11 +27,12 @@ func main() {
 		result := []string{}
 		for _, x := range imageList.images {
 			distance := img.Compare(x)
-			if distance > 220 {
-				result = append(result, x.filename + ": " + strconv.Itoa(distance))
+			if distance < 220 {
+				result = append(result, "\r\n"+ "path: " + x.path +
+								", filename: " + x.filename + ": " + strconv.Itoa(distance))
 			}
 		}
-		fmt.Println(img.filename+ ":" + strings.Join(result, ""))
+		fmt.Println("--------" + img.filename + "--------" + strings.Join(result, ""))
 	}
 	
 }
@@ -49,56 +51,68 @@ func checkError(e error) {
 Получаем информацию о файле
  */
 type Image struct {
-	filename string
+	path, filename string
 	t_data [] int
 }
 
-func NewImage(fileName string) *Image {
+func NewImage(path string, fileName string) *Image {
 	img := new(Image)
 	img.filename = fileName
+	img.path = path
 	return img
 }
 
 func(o *Image) Load() *Image {
-	fileLoad, fileError := ioutil.ReadFile(o.filename)
+	fileLoad, fileError := ioutil.ReadFile(o.path + string(os.PathSeparator) + o.filename)
 	checkError(fileError)
 	
 	img, imgError := jpeg.Decode(bytes.NewReader(fileLoad))
 	checkError(imgError)
 	
-	small := resize.Resize(BLOCK_SIZE, BLOCK_SIZE, img, resize.Bilinear)
-	bounds := small.Bounds()
+	dstImage := image.NewRGBA(image.NewRGBA(image.Rect(0, 0, BLOCK_SIZE, BLOCK_SIZE)).Bounds())
+	
+	small := gift.New(
+		gift.ResizeToFill(BLOCK_SIZE, BLOCK_SIZE, gift.LanczosResampling, gift.CenterAnchor),
+		gift.Grayscale(),
+		gift.Sobel())
+	
+	small.Draw(dstImage, img)
+	
+	out, err := os.Create("tmp-"+o.filename)
+	checkError(err)
+	defer out.Close()
+	jpeg.Encode(out, dstImage, nil)
+	
+	bounds := dstImage.Bounds()
+	
 	for x := bounds.Min.X; x < bounds.Max.X; x++ {
 		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-			r, g, b, _ := small.At(x, y).RGBA()
-			o.t_data = append(o.t_data, int(uint8(r) + uint8(g) + uint8(b)))
+			r, g, b, _ := dstImage.At(x, y).RGBA()
+			o.t_data = append(o.t_data, int((uint8(r) + uint8(g) + uint8(b))))
 		}
 	}
 	
 	return o
 }
 
+/** Находим растояние между картинками
+ */
 func(o *Image) Compare(other Image) int {
-	var result int = 0
-	data := difference(o.t_data, other.t_data)
-	for _, x := range data {
-		if(x > THRESHOLD) {
-			result += x
+	return difference(o.t_data, other.t_data)
+}
+
+func difference(data1 []int, data2 []int) int {
+	result := 0
+	
+	if len(data1) == len(data2) {
+		for i := 0; i < len(data1); i++ {
+			t := int(math.Abs(float64(data1[i] - data2[i])))
+			if(t > THRESHOLD) {
+				result++
+			}
 		}
 	}
 	return result
-}
-
-func difference(data1 []int, data2 []int) []int {
-	var diff []int
-	if len(data1) == len(data2) {
-		for i := 0; i < len(data1); i++ {
-			t := math.Abs(float64(data1[i] - data2[i]))
-			
-			diff = append(diff, int(t))
-		}
-	}
-	return diff
 }
 
 /** Подгружаем файлы из директории
@@ -120,10 +134,15 @@ func(o *ImageList) Load () *ImageList  {
 	files, _ := ioutil.ReadDir(o.dirname)
 	for _, file := range files {
 		if strings.HasSuffix(file.Name(), ".jpg") {
-			fullPath := o.dirname + string(os.PathSeparator) + file.Name()
-			o.images = append(o.images, *NewImage(fullPath).Load())
+			o.images = append(o.images, *NewImage(o.dirname, file.Name()).Load())
 		}
 	}
 	
 	return o
 }
+/*out, err := os.Create(o.outFileName)
+	checkError(err)
+	defer out.Close()*/
+
+// write new image to file
+//jpeg.Encode(out, small, nil)
